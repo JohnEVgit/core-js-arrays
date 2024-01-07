a2c46a02da22cbdf06704dba3b9dbf3b0f7d593f
Yauheni Paloika <john13ev@gmail.com>
2024-01-07 19:05:19 +0400

    feat: add selectMany

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -310,9 +310,9 @@ const flattenArray = (nestedArray) => {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
-}
+const selectMany = (arr, childrenSelector) => {
+  return arr.flatMap(childrenSelector);
+};
 
 /**
  * Every month, you record your income and expenses.
