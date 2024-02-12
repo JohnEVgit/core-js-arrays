9f6f241d03932e38119cb714cd66c0da2186e041
Yauheni Paloika <john13ev@gmail.com>
2024-02-13 01:22:06 +0400

    feat: add getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -395,9 +395,9 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
-}
+const getFalsyValuesCount = (arr) => {
+  return arr.filter((elem) => !elem).length;
+};
 
 /**
  * Creates an identity matrix of the specified size.
